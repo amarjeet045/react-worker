9991461ec656e7f0295f7132f1df251be378eb4a
amarjeet <singhamarjeet0045@gmail.com>
2022-06-15 07:23:30 +0530

    after adding the vendor onboard

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import AuthProvider from "./contexts/AuthContext";
 function App() {
   return (
     <AuthProvider>
-    <BrowserRouter basename="/React">
+    <BrowserRouter basename="/">
      
         <Routes>
         <Route path="/" element={<SuccessMessage />} />r
